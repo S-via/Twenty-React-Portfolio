f334b8766c8280a59af3a0b45bece750f44185ce
S-via <168160925+S-via@users.noreply.github.com>
2024-11-15 15:05:07 -0800

    added logic inisde header and imoprted navigation component

--- twenty-react/src/components/Header.jsx
+++ twenty-react/src/components/Header.jsx
@@ -1,1 +1,14 @@
-// header does not change  
+// header does not change 
+
+// import component from Navigation
+import Navigation from "./Navigation";
+
+function Header() {
+    return (
+        <header>
+            My Portfolio
+            <Navigation/>
+        </header>
+    )
+}
+export default Header;

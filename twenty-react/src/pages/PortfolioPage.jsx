6585770fb8df9378d7ff0cdd4da612be8c559021
S-via <168160925+S-via@users.noreply.github.com>
2024-11-16 15:24:16 -0800

    added image, anchor tag into portfoliopage

--- twenty-react/src/pages/PortfolioPage.jsx
+++ twenty-react/src/pages/PortfolioPage.jsx
@@ -1,8 +1,16 @@
 /* titled images of six of the developer’s applications 
 with links to both the deployed applications and the corresponding GitHub repositories  */
+import React from 'react';
+import myImage from '../assets/blogyou.png'
 export default function Portfolio() {
     return(
         <div>
+            <h3>Portoflio Title</h3>
+           {/*  add hover css over images */}
+           {/* fix sizing image  */}
+            <a href ="https://s-via.github.io/Four-Personal-Blog/">
+            <img src ={myImage} atl='myImage'/>
+            </a>
             <p> 
                 this is portfolio page content 
                 this is portfolio page content 

7cc3f9614ac13c81ed83c6df920134e00c90893c
S-via <168160925+S-via@users.noreply.github.com>
2024-11-18 14:21:28 -0800

    adjusted diaply for all pages

--- twenty-react/src/pages/AboutmePage.jsx
+++ twenty-react/src/pages/AboutmePage.jsx
@@ -1,22 +1,12 @@
 export default function AboutmePage () {
     return (
-<div className="heading-container">
+<div className="aboutme-container">
         <h2>Heading</h2>
         {/* Photo avatar goes here */}
         <p>
             This is about me content
             This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
-            This is about me content
+           
 
         </p>
         </div>

da3e53f8ec161339bd52b4b4b18f083877ef3f8a
S-via <168160925+S-via@users.noreply.github.com>
2024-11-17 16:13:33 -0800

    contactpage is finshed with function logic

--- twenty-react/src/pages/ContactPage.jsx
+++ twenty-react/src/pages/ContactPage.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 export default function ContactPage() {
     // form logic 
     // from xpert learning example
+    const [alert, setAlert] = useState('')
     const [formData, setData] = useState({
         name: '',
         email: '',
@@ -12,21 +13,33 @@ export default function ContactPage() {
     const handleChange = (e) => {
         const { name, value } = e.target;
         console.log(name, value)
+
         setData({
             ...formData,
             [name]: value
         })
+        console.log(formData)
 
     }
 
     const handlerSubmit = (e) => {
         e.preventDefault();
+        console.log('form submitted')
+        if (!formData.name || !formData.email || !formData.message) {
+            console.log('before return');
+            setAlert('Fields are required!')
+            return;
+        }
+
         setData({
             name: '',
             email: '',
             message: ''
-        });
+        })
+        setAlert('')
+        console.log('last of fucntion ')
     }
+
 
     return (
         // what it will return 
@@ -39,21 +52,22 @@ export default function ContactPage() {
                     name='name'
                     value={formData.name}
                     onChange={handleChange}
-                    required />
+                />
                 Email:
                 <input
                     type='email'
                     name='email'
                     value={formData.email}
                     onChange={handleChange}
-                    required />
+                />
                 Message:
                 <input
                     type='text'
                     name='message'
                     value={formData.message}
                     onChange={handleChange}
-                    required />
+                />
+                {alert && <div style={{ color: 'red' }}>{alert}</div>}
 
                 <button type="submit">
                     Submit

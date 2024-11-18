b65d5a8e0ee3c0cf53bf4541e3ea37945b3951e1
S-via <168160925+S-via@users.noreply.github.com>
2024-11-18 00:36:49 -0800

    added logic for pdf download

--- twenty-react/src/pages/ResumePage.jsx
+++ twenty-react/src/pages/ResumePage.jsx
@@ -1,8 +1,15 @@
 // dowloadable resume 
+import React from 'react';
+import myPdf from '../assets/resume.pdf'
+
 export default function ResumePage () {
+
+
     return(
         <div>
             <h2>Resume Heading</h2>
+            <p>Download my <a href={myPdf} download='resume.pdf'>Resume</a></p>
+
         </div>
     )
 }

a5714928be9482a309b6072491c7c368e58ee16c
S-via <168160925+S-via@users.noreply.github.com>
2024-11-20 11:55:17 -0800

    added in about me

--- twenty-react/src/pages/AboutmePage.jsx
+++ twenty-react/src/pages/AboutmePage.jsx
@@ -3,7 +3,15 @@ export default function AboutmePage() {
         <div className="aboutme-container">
             <h2>About Me</h2>
             {/* Photo avatar goes here */}
-            <p>
+
+            <p>Im someone who always driven by curiosity and a genuine desire to grow.
+                I love learning new things and challenging myself to think differently. I do my best work in 
+                places that value creativity, fresh ideas, and collaboraation- it's exciting to be part of growth.
+
+                When im not working, you'll probably find me enjoying outdoors, getting lost ina good book, or diving into a creative project. Whether its something hands-on or just
+                experimenting with new ideas, these moments help me recharge and keep my perspective fresh.
+
+                For me, staying curios and creative isnt just a part of my work -it's how i approach life 
               
 
 

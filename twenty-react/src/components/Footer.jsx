06027c17242cbdefec9cccac7e0d5fa840298eaa
S-via <168160925+S-via@users.noreply.github.com>
2024-11-21 18:15:59 -0800

    added svg logo

--- twenty-react/src/components/Footer.jsx
+++ twenty-react/src/components/Footer.jsx
@@ -1,16 +1,15 @@
 // footer does not change 
 import gitLogo from "../assets/github-mark.svg"
-import linkIn from "../assets/icon8-linkedin-48.png"
+
 function Footer () {
     return (
         <footer>
-            Footer .. 
+            
             <a href="https://github.com/S-via">
             <img src ={gitLogo} style={{width:"50px",height:"auto"}}/></a>
 
-            <a href="https://www.linkedin.com/in/silvia-villalba-48a28b2b2/">
-            <img src={linkIn} style ={{width:"50px", height:"auto"}}/></a>
-            
+            <a href="https://www.linkedin.com/in/silvia-villalba-48a28b2b2/">linken</a>
+
             <a href="">StackOverFlow</a>
             
         </footer>
